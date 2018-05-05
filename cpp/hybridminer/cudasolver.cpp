ccdf449198af48bc4eb2f6a25ac3c35db8f522a8
Azlehria <azlehria@gmail.com>
2018-05-05 09:01:23 -0700

    Lambdas are cool, but unnecessary here

--- cpp/hybridminer/cudasolver.cpp
+++ cpp/hybridminer/cudasolver.cpp
@@ -8,16 +8,8 @@ CUDASolver::CUDASolver( int32_t device, double intensity ) noexcept :
 m_stop( false ),
 m_new_target( true ),
 m_new_message( true ),
-m_intensity( [=]() -> double {
-    return intensity <= 41.99 ? intensity : 41.99;
-  }() ),
-m_threads( [=]() -> uint64_t {
-    if( intensity <= 41.99 )
-    {
-      return static_cast<uint64_t>(std::pow( 2, intensity ));
-    }
-    return static_cast<uint64_t>(std::pow( 2, 41.99 ));
-  }() ),
+m_intensity( intensity <= 41.99 ? intensity : 41.99 ),
+m_threads( static_cast<uint64_t>(std::pow( 2, intensity <= 41.99 ? intensity : 41.99 )) ),
 m_device_failure_count( 0u ),
 m_gpu_initialized( false ),
 m_device( device ),
